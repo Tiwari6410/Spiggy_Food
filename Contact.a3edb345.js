241e1912c7c5345435f7e159adddd499ca195156
your name <your.name@domain.com>
2023-11-19 09:08:56 +0530

    Updates

---
+++ Contact.a3edb345.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire4dce;(0,e.register)("f86b2",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0");e("acw62");var t=()=>/*#__PURE__*/(0,i.jsx)("div",{children:/*#__PURE__*/(0,i.jsx)("h1",{children:"This is contact us page\uD83D\uDCDE\uD83D\uDCF2"})})});//# sourceMappingURL=Contact.a3edb345.js.map
+
+//# sourceMappingURL=Contact.a3edb345.js.map
---
+++ Grocery.8e2cb72f.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire4dce;(0,e.register)("2fhM2",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0");e("acw62");var l=()=>/*#__PURE__*/(0,a.jsx)("div",{children:/*#__PURE__*/(0,a.jsx)("h1",{children:"This is all About Grocery Store"})})});//# sourceMappingURL=Grocery.8e2cb72f.js.map
+
+//# sourceMappingURL=Grocery.8e2cb72f.js.map
