cdb45f36df426d0409dff4b07e072381191e21cf
your name <your.name@domain.com>
2023-11-19 09:59:37 +0530

    Error Page change

--- src/Component/Error.jsx
+++ src/Component/Error.jsx
@@ -5,12 +5,14 @@ const Error =()=>{
     const err= useRouteError();
     console.log(err)
     return(
-        <div className="container flex justify-center items-center" w-auto style={{height:"768px"}}>
+        <div className="container flex justify-center items-center bg-slate-25" w-auto style={{height:"768px"}}>
             <div className="center">
             <img src="https://i.pinimg.com/originals/c5/7f/5a/c57f5af94487bb62711c5adf7926feb2.gif" width={400} height={300} alt="" />
-            <h1>Opps</h1>
-            <h1>You went something wrong.........🚀</h1>
+            <div className="text-center">
+            <h1 className="font-bold text-3xl mb-3">Opps</h1>
+            <h1 className="">You went something wrong.........🚀</h1>
             <h3>{err.status}:{err.statusText}</h3>
+            </div>
             </div>
            
         </div>
