90c7b0733167467b91bd0d65fc9588c9f5c7cdff
your name <your.name@domain.com>
2023-11-19 09:23:31 +0530

    fourth commit

--- src/Component/Error.jsx
+++ src/Component/Error.jsx
@@ -5,9 +5,9 @@ const Error =()=>{
     const err= useRouteError();
     console.log(err)
     return(
-        <div className="container flex justify-center items-center bg-slate-100" w-auto style={{height:"768px"}}>
+        <div className="container flex justify-center items-center" w-auto style={{height:"768px"}}>
             <div className="center">
-            <img src="https://i.gifer.com/Qw5v.gif" width={400} height={300} alt="" />
+            <img src="https://i.pinimg.com/originals/c5/7f/5a/c57f5af94487bb62711c5adf7926feb2.gif" width={400} height={300} alt="" />
             <h1>Opps</h1>
             <h1>You went something wrong.........🚀</h1>
             <h3>{err.status}:{err.statusText}</h3>
